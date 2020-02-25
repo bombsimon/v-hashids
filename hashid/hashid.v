2031837a0c5d682278b2c1940b558769b7c75bfc
Simon Sawert <simon@sawert.se>
2020-02-25 22:28:44 +0100

    Add remove_in and remove_not_in

--- hashid/hashid.v
+++ hashid/hashid.v
@@ -2,19 +2,22 @@ module hashid
 
 const (
     version = "1.0.0"
-    default_alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    default_alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     default_seps = "cfhistuCFHISTU"
     default_min_alphabet_length = 16
+    default_min_hash_length = 0
+    default_salt = ""
 )
 
 struct HashID {
     alphabet string
-    min_length int
+    seps string
     salt string
+    min_length int
 }
 
 pub fn new() HashID {
-    return new_with_config(default_alphabet, 0, "")
+    return new_with_config(default_alphabet, default_min_hash_length, default_salt)
 }
 
 pub fn new_with_config(alphabet string, min_length int, salt string) HashID {
@@ -24,10 +27,14 @@ pub fn new_with_config(alphabet string, min_length int, salt string) HashID {
         panic("too short alphabet")
     }
 
+    alphabet_without_seps := remove_in(unique, default_seps)
+    seps_in_alphabet := remove_not_in(default_seps, unique)
+
     return HashID{
-        alphabet: unique,
-        min_length: min_length,
+        alphabet: alphabet_without_seps,
+        seps: seps_in_alphabet,
         salt: salt
+        min_length: min_length,
     }
 }
 
@@ -55,4 +62,36 @@ fn unique_alphabet(alphabet string) string {
     unique.sort()
 
     return unique.join("")
+}
+
+fn remove_in(a, b string) string {
+    a_arr := a.split("")
+    b_arr := b.split("")
+    mut final_arr := []string
+
+    for x in a_arr {
+        if x in b_arr {
+            continue
+        }
+
+        final_arr << x
+    }
+
+    return final_arr.join("")
+}
+
+fn remove_not_in(a, b string) string {
+    a_arr := a.split("")
+    b_arr := b.split("")
+    mut final_arr := []string
+
+    for x in a_arr {
+        if !x in b_arr {
+            continue
+        }
+
+        final_arr << x
+    }
+
+    return final_arr.join("")
 }
--- hashid/hashid_test.v
+++ hashid/hashid_test.v
@@ -1,8 +1,32 @@
 module hashid
 
+fn test_new_with_config() {
+    hid := new_with_config(
+        default_alphabet,
+        default_min_hash_length,
+        "my salt"
+    )
+
+    assert hid.alphabet == remove_in(default_alphabet, default_seps)
+    assert hid.min_length == default_min_hash_length
+    assert hid.salt == "my salt"
+}
+
 fn test_unique_alphabet() {
     assert unique_alphabet("abc") == "abc"
     assert unique_alphabet("aaabbcc") == "abc"
     assert unique_alphabet("aaaaaabbccccaaa") == "abc"
     assert unique_alphabet("abc123abc123") == "123abc"
+}
+
+fn test_remove_in() {
+    assert remove_in("abcde", "de") == "abc"
+    assert remove_in("abc", "abc") == ""
+    assert remove_in("abc", "xyz") == "abc"
+}
+
+fn test_remove_not_in() {
+    assert remove_not_in("abcd", "abc") == "abc"
+    assert remove_not_in("abc", "def") == ""
+    assert remove_not_in("aabbcc", "axy") == "aa"
 }
